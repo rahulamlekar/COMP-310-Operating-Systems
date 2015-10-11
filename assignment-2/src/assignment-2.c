854e69f415287e419169dc51780071f6834a545d
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2015-10-11 12:53:59 -0400

    New: Early code framework

--- assignment-2/src/assignment-2.c
+++ assignment-2/src/assignment-2.c
@@ -10,8 +10,56 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "fifo_buffer.h"
+#include "print_job.h"
+
+void setup_shared_mem() {
+
+}
+
+void attach_shared_mem() {
+
+}
+
+void init_semaphore() {
+
+}
+
+void take_a_job(PrintJob* job) {
+
+}
+
+/**
+ * Print the properties of a job.
+ */
+void print_a_msg(PrintJob* job) {
+
+}
+
+/**
+ * Sleep for the duration of a job.
+ */
+void go_sleep(PrintJob* job) {
+	printf("Starting to sleep.\n");
+	sleep(job->duration);
+	printf("Finished sleeping.\n");
+}
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	setup_shared_mem();    // create a shared memory segment
+	attach_shared_mem();   // attach the shared memory segment
+	init_semaphore();      // initialize the semaphore and put it in shared memory
+
+	PrintJob job = {
+			0,
+			0
+	};
+
+	while (1) {
+		take_a_job(&job);  // this is blocking on a semaphore if no job
+		print_a_msg(&job); // duration of job, job ID, source of job are printed
+		go_sleep(&job);    // sleep for job duration
+	}
+
 	return EXIT_SUCCESS;
 }

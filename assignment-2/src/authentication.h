2dee2419d6605b88023b282a5a983937e403cfbd
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2015-10-11 13:39:41 -0400

    New: Shared auth key

---
+++ assignment-2/src/authentication.h
@@ -0,0 +1,13 @@
+/*
+ * authentication.h
+ *
+ *  Created on: Oct 11, 2015
+ *      Author: Andrew
+ */
+
+#ifndef AUTHENTICATION_H_
+#define AUTHENTICATION_H_
+
+#define SHARED_MEM_KEY 123456789
+
+#endif /* AUTHENTICATION_H_ */

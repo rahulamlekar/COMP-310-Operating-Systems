925e5f5b5290b20bb1b810cd7891ba3c6bf6b25e
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2015-10-11 12:20:26 -0400

    New: Assignment 2 hello world

---
+++ assignment-2/src/assignment-2.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : assignment-2.c
+ Author      : Andrew Fogarty - 260535895
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}

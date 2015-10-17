f812d6beec84e6a1d9bff6d19b957f6024d981fe
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2015-10-17 18:20:01 -0400

    Fixed: All buffer ops just go in H file

--- assignment-2/src/fifo_buffer_ops.h
+++ assignment-2/src/fifo_buffer_ops.h
@@ -10,7 +10,37 @@
 #ifndef FIFO_BUFFER_OPS_H_
 #define FIFO_BUFFER_OPS_H_
 
-extern int pushFifoBuffer(FifoBuffer* buffer, PrintJob* newValue);
-extern PrintJob* popFifoBuffer(FifoBuffer* buffer);
+int isBufferEmpty(FifoBuffer* buffer) {
+    return buffer->numberOfMembers <= 0;
+}
+
+int isBufferFull(FifoBuffer* buffer) {
+
+}
+
+int pushFifoBuffer(FifoBuffer* buffer, PrintJob* newValue) {
+    if (!buffer->elementFull[buffer->tailIndex]) {
+        // We can proceed
+        buffer->elementFull[buffer->tailIndex] = 1;
+        buffer->elements[buffer->tailIndex] = newValue;
+        buffer->tailIndex = (buffer->tailIndex + 1) % BUFFER_SIZE;
+        // It worked!
+        return 1;
+    }
+
+    // It didn't work
+    return 0;
+}
+
+PrintJob* popFifoBuffer(FifoBuffer* buffer) {
+    // Grab the element we will take
+    PrintJob* output = buffer->elements[buffer->headIndex];
+    // Mark it as empty
+    buffer->elementFull[buffer->headIndex] = 0;
+    // Move the index along
+    buffer->headIndex = (buffer->headIndex + 1) % BUFFER_SIZE;
+    // Return the value
+    return output;
+}
 
 #endif /* FIFO_BUFFER_OPS_H_ */

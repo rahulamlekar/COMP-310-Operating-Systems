cd83e14f4b4e1c0e08bdf55b82598fe93545cc97
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2015-10-21 17:17:05 -0400

    New: Works with Make

---
+++ assignment-3/src/sfs_api.c
@@ -0,0 +1,82 @@
+//
+// Created by Andrew on 2015-10-21.
+//
+
+#include "sfs_api.h"
+
+
+/**
+ * creates the file system
+ */
+void mksfs(int fresh) {
+    // TODO
+
+}
+
+
+/**
+ * Get the name of the next file in directory
+ */
+int sfs_getnextfilename(char *fname) {
+    // TODO
+    return -1;
+}
+
+
+/**
+ * Get the size of the given file
+ */
+int sfs_getfilesize(const char* path) {
+    // TODO
+    return -1;
+}
+
+
+/**
+ * opens the given file
+ */
+int sfs_fopen(char *name) {
+    // TODO
+    return -1;
+}
+
+
+/**
+ * Closes the given file
+ */
+void sfs_fclose(int fileID) {
+    // TODO
+}
+
+
+/**
+ * Write buf characters into disk
+ */
+void sfs_fwrite(int fileID, char *buf, int length) {
+    // TODO
+}
+
+
+/**
+ * read characters from disk into buf
+ */
+void sfs_fread(int fileID, char *buf, int length) {
+    // TODO
+}
+
+
+/**
+ * seek to the location from beginning
+ */
+void sfs_fseek(int fileID, int loc) {
+    // TODO
+}
+
+
+/**
+ * removes a file from the filesystem
+ */
+int sfs_remove(char *file) {
+    // TODO
+    return -1;
+}
--- assignment-3/src/sfs_api.h
+++ assignment-3/src/sfs_api.h
@@ -5,80 +5,14 @@
 #ifndef ASSIGNMENT_3_SFS_API_H
 #define ASSIGNMENT_3_SFS_API_H
 
-/**
- * creates the file system
- */
-void mksfs(int fresh) {
-    // TODO
-}
-
-
-/**
- * Get the name of the next file in directory
- */
-int sfs_getnextfilename(char *fname) {
-    // TODO
-    return -1;
-}
-
-
-/**
- * Get the size of the given file
- */
-int sfs_getfilesize(const char* path) {
-    // TODO
-    return -1;
-}
-
-
-/**
- * opens the given file
- */
-int sfs_fopen(char *name) {
-    // TODO
-    return -1;
-}
-
-
-/**
- * Closes the given file
- */
-void sfs_fclose(int fileID) {
-    // TODO
-}
-
-
-/**
- * Write buf characters into disk
- */
-void sfs_fwrite(int fileID, char *buf, int length) {
-    // TODO
-}
-
-
-/**
- * read characters from disk into buf
- */
-void sfs_fread(int fileID, char *buf, int length) {
-    // TODO
-}
-
-
-/**
- * seek to the location from beginning
- */
-void sfs_fseek(int fileID, int loc) {
-    // TODO
-}
-
-
-/**
- * removes a file from the filesystem
- */
-int sfs_remove(char *file) {
-    // TODO
-    return -1;
-}
-
+extern void mksfs(int fresh); // creates the file system
+extern int sfs_getnextfilename(char *fname); // get the name of the next file in directory
+extern int sfs_getfilesize(const char* path); // get the size of the given file
+extern int sfs_fopen(char *name); // opens the given file
+extern void sfs_fclose(int fileID); // closes the given file
+extern void sfs_fwrite(int fileID, char *buf, int length); // write buf characters into disk
+extern void sfs_fread(int fileID, char *buf, int length); // read characters from disk into buf
+extern void sfs_fseek(int fileID, int loc); // seek to the location from beginning
+extern int sfs_remove(char *file); // removes a file from the filesystem
 
 #endif //ASSIGNMENT_3_SFS_API_H

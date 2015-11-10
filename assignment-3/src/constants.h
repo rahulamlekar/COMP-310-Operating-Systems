e62b5787fb734e01e6ffe529b62a8a72f20f92d7
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2015-11-10 15:17:35 -0500

    Fixed: Don't write the free bitmap to one of the data blocks every time!!!

--- assignment-3/src/constants.h
+++ assignment-3/src/constants.h
@@ -17,14 +17,14 @@
 // Constants related to disk stuff
 #define SB_MAGIC 0xAABB0005
 #define DISK_BLOCK_SIZE 512
-#define DISK_BLOCK_COUNT 256
+#define DISK_BLOCK_COUNT 2048
 #define FILE_SYSTEM_NAME "tmp4.disk"
 
 // Disk indices of structures
 #define SUPER_BLOCK_INDEX 0
 #define I_NODE_TABLE_BLOCK_INDEX 1
-#define DATA_BLOCK_TABLE_INDEX 16
+#define DATA_BLOCK_TABLE_INDEX 17
 #define DIRECTORIES_BLOCK_INDEX 32
-#define FREE_BITMAP_BLOCK_INDEX 64
+#define FREE_BITMAP_BLOCK_INDEX 2000 // If this isn't high enough, then it overwrites data blocks!!!
 
 #endif //ASSIGNMENT_3_CONSTANTS_H
